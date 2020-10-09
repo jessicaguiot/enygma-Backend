a3f059c309d2aaf32eadda7b7aba6102b669125f
jessicaguiot <jessica.guiot97@gmail.com>
2020-10-09 10:13:53 -0300

    creating read function for seasons

--- Sources/App/Controllers/SeasonController.swift
+++ Sources/App/Controllers/SeasonController.swift
@@ -6,6 +6,7 @@ struct SeasonController: RouteCollection {
         let seasons = routes.grouped("seasons")
         seasons.get(use: index)
         seasons.post(use: create)
+        seasons.get(":id", use: read)
         seasons.group(":seasonID") { season in
             season.delete(use: delete)
         }
@@ -14,7 +15,18 @@ struct SeasonController: RouteCollection {
     func index(req: Request) throws -> EventLoopFuture<[Season]> {
         return Season.query(on: req.db).all()
     }
-
+    
+    func read(req: Request) throws -> EventLoopFuture<Season> {
+        
+        guard let id = req.parameters.get("id", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+        
+        return Season.find(id, on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .map { Season(id: $0.id, name: $0.name , theme: $0.theme, background: $0.background, soundTrackURL: $0.soundTrackURL) }
+    }
+ 
     func create(req: Request) throws -> EventLoopFuture<Season> {
         let season = try req.content.decode(Season.self)
         return season.save(on: req.db).map { season }
